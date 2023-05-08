8d1b9873266a58cc74b100f9e91d7e783ddd4817
phuochungus <21520252@gm.uit.edu.vn>
2023-05-08 15:15:50 +0700

    change oauth for web deploy heroku

--- src/auth/strategies/google-oauth2.strategy.ts
+++ src/auth/strategies/google-oauth2.strategy.ts
@@ -11,7 +11,9 @@ export default class GoogleStrategy extends PassportStrategy(
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google-redirect',
+      callbackURL:
+        (process.env.HEROKU_APP_URL || 'http://localhost:3000') +
+        '/auth/google-redirect',
       scope: ['email', 'profile'],
     });
   }
