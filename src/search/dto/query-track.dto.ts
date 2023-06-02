7519fb3d61fd3bef097f8ea13b6bb39f2391985c
phuochungus <21520252@gm.uit.edu.vn>
2023-06-02 14:54:43 +0700

    change default export

--- src/search/dto/query-track.dto.ts
+++ src/search/dto/query-track.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional, IsString, Min } from 'class-validator';
 
-export default class QueryTrackDTO {
-  @ApiProperty({example:'Đông kiếm em'})
+export class QueryTrackDTO {
+  @ApiProperty({ example: 'Đông kiếm em' })
   @IsString()
   query: string;
 
