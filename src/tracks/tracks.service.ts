8550f496bdf909ce16920a4f8b578abea0c14b18
phuochungus <21520252@gm.uit.edu.vn>
2023-06-12 14:19:19 +0700

    add files

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -86,7 +86,8 @@ export class TracksService {
                 this.supabase.storage
                   .from('tracks')
                   .upload(spotifyId, data, { contentType: 'audio/ogg' });
-
+                  
+                response.setHeader('Content-Type', 'audio/ogg');
                 response.send(data);
                 // .then(async () => {
                 //   const { data, error } = await this.supabase.storage
