ee15d83e260535bd1aecf38fd45b0c7171a5db6f
phuochungus <21520252@gm.uit.edu.vn>
2023-06-14 00:31:30 +0700

    add !

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -28,8 +28,8 @@ export class TracksService {
   ) {}
 
   private supabase = createClient(
-    process.env.SUPABASE_PROJECT_URL,
-    process.env.SUPABASE_API_KEY,
+    process.env.SUPABASE_PROJECT_URL!,
+    process.env.SUPABASE_API_KEY!,
   );
 
   async play(spotifyId: string, response: Response) {
