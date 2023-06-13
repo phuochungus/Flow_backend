f4c040f851da17830825320304406564f6b00573
phuochungus <21520252@gm.uit.edu.vn>
2023-06-13 23:54:07 +0700

    add cache module using redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MeModule } from './me/me.module';
@@ -15,11 +15,25 @@ import { YoutubeApiModule } from './youtube-api/youtube-api.module';
 import { SpotifyToYoutubeModule } from './spotify-to-youtube/spotify-to-youtube.module';
 import { SeminarModule } from './seminar/seminar.module';
 import { LyricsModule } from './lyrics/lyrics.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        store: await redisStore({ url: config.get<string>('REDIS_URL') }),
+        ttl: 6 * 24 * 1000,
+        isGlobal: true,
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URL, { autoIndex: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URL || 'localhost:27017', {
+      autoIndex: true,
+    }),
     AuthModule,
     UsersModule,
     MeModule,
