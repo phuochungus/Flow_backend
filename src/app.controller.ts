501f99cf3d3f4a813d88fe50ded718ca939aabff
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-06-29 00:46:33 +0700

    add ping GET

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('/ping')
+  pong() {
+    return 'pong';
+  }
+}
