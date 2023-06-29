468d97db81c615965710dca9188550e366831eb6
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-06-29 19:35:43 +0700

    implement cache manually, remove interceptor

--- src/lyrics/lyrics.service.ts
+++ src/lyrics/lyrics.service.ts
@@ -1,8 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TrackLyrics } from './schemas/lyric.schema';
 import { SpotifyApiService } from '../spotify-api/spotify-api.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class LyricsService {
@@ -10,9 +17,16 @@ export class LyricsService {
     @InjectModel(TrackLyrics.name)
     private trackLyricsModel: Model<TrackLyrics>,
     private spotifyApi: SpotifyApiService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async findOne(id: string) {
+    const cacheResult = await this.cacheManager.get(`lyrics_${id}`);
+    if (cacheResult) {
+      if (cacheResult != 'null') return cacheResult;
+      else throw new NotFoundException();
+    }
+
     const trackLyrics = await this.trackLyricsModel
       .findOne({ trackId: id }, { _id: false })
       .lean();
@@ -20,6 +34,7 @@ export class LyricsService {
       if (trackLyrics.lyrics) return trackLyrics.lyrics;
       throw new NotFoundException();
     }
+
     try {
       const lyrics = await this.spotifyApi.getLyric(id);
       const createdDocument = new this.trackLyricsModel({
@@ -28,13 +43,15 @@ export class LyricsService {
       });
 
       createdDocument.save();
+      if (lyrics) this.cacheManager.set(`lyrics_${id}`, lyrics);
+      else this.cacheManager.set(`lyrics_${id}`, 'null');
       if (lyrics) {
         return lyrics;
       } else {
         throw new NotFoundException();
       }
     } catch (error) {
-      console.error(error);
+      if (!(error instanceof HttpException)) console.error(error);
       throw error;
     }
   }
