6f5cb61ebb66d26232d9e3c53228ae7b5667aad5
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-06-30 22:01:56 +0700

    moving instantiate responsible to module declare class

--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -4,11 +4,21 @@ import { TracksController } from './tracks.controller';
 import { SpotifyApiModule } from 'src/spotify-api/spotify-api.module';
 import { YoutubeApiModule } from 'src/youtube-api/youtube-api.module';
 import { SpotifyToYoutubeModule } from 'src/spotify-to-youtube/spotify-to-youtube.module';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
 @Module({
   imports: [SpotifyApiModule, YoutubeApiModule, SpotifyToYoutubeModule],
   controllers: [TracksController],
-  providers: [TracksService],
+  providers: [
+    TracksService,
+    {
+      provide: SupabaseClient,
+      useValue: createClient(
+        process.env.SUPABASE_PROJECT_URL!,
+        process.env.SUPABASE_API_KEY!,
+      ),
+    },
+  ],
   exports: [TracksService],
 })
 export class TracksModule {}
