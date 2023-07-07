28d122d2add639ce60822ff29469f67d5169c49c
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-07-07 11:11:03 +0700

    fix authorize server

--- src/spotify-api/spotify-api.service.ts
+++ src/spotify-api/spotify-api.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import SpotifyWebApi from 'spotify-web-api-node';
 import _ from 'lodash';
-import { APIWrapper } from '../abstract/abstract';
 
 @Injectable()
-export class SpotifyApiService implements APIWrapper {
+export class SpotifyApiService {
   constructor(public spotifyWebApi: SpotifyWebApi) {
-    this.authorize();
-    setInterval(this.authorize, 3300000);
+    this.authorize(spotifyWebApi);
+    setInterval(() => this.authorize(spotifyWebApi), 3300000);
   }
 
-  async authorize() {
-    this.spotifyWebApi.clientCredentialsGrant().then((data: any) => {
-      this.spotifyWebApi.setAccessToken(data.body.access_token);
+  async authorize(spotifyApiInstance: any) {
+    spotifyApiInstance.clientCredentialsGrant().then((data: any) => {
+      spotifyApiInstance.setAccessToken(data.body.access_token);
     });
   }
 }
